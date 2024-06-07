80899e3ede79f7a4805e1a73de34e25ec5ff18ff
Shiv  Ram <shivramkgithub@gmail.com>
2024-06-07 12:11:15 +0530

    Create alert.sh

---
+++ DISK-STORAGE-MEMORY-USAGE-CPU-UTILIZATON-ALERT-SHELL-SCRIPT/alert.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+        DLIMIT=70
+        MLIMIT=75
+        CLIMIT=2.85
+	DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}' )
+        MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
+	CPU=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}') 
+	
+
+ if [[ "$DISK" >  "$DLIMIT" ]]; then
+
+	 mail -s 'Disk Space Alert' shellbashscripts@gmail.com << EOF
+
+	Your root partition remaining free space is critically low. Used Storage: $DISK
+
+EOF
+fi
+
+if [[ "$MEMORY" >  "$MLIMIT" ]];  then
+
+        mail -s 'Memory Space Alert' shellbashscripts@gmail.com << EOF
+
+       Your Memory  free space is critically low. Used Memory: $MEMORY
+		
+EOF
+fi
+
+if [[ "$CPU" >  "$CLIMIT" ]];  then
+
+  	mail -s 'CPU Space Alert' shellbashscripts@gmail.com << EOF
+
+      Your CPU Load is high.Current average load : $CPU
+
+EOF
+fi
